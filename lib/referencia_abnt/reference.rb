ea16cbbb034aec167c91e3000f76475697d54c2f
Rodrigo Manhães <rmanhaes@gmail.com>
2012-06-09 21:54:45 -0300

    add support to subtitles

--- lib/referencia_abnt/reference.rb
+++ lib/referencia_abnt/reference.rb
@@ -1,11 +1,24 @@
 class ABNTReference
   def initialize(options)
     @reference = "%s %s. %s: %s, %s." % [
-      Author.new(options[:author]), options[:title], options[:address],
-      options[:publisher], options[:year]]
+      Author.new(options[:author]),
+      make_title(options[:title], options[:subtitle]),
+      options[:address],
+      options[:publisher],
+      options[:year]]
   end
 
   def to_s
     @reference
+  end
+
+  private
+
+  def make_title(title, subtitle)
+    title + make_subtitle(subtitle)
+  end
+
+  def make_subtitle(subtitle)
+    subtitle ? ": #{subtitle.downcase.capitalize}" : ''
   end
 end
--- spec/referencia_abnt_spec.rb
+++ spec/referencia_abnt_spec.rb
@@ -12,4 +12,15 @@ describe ABNTReference do
       year: 1998).to_s.should ==
     'GOMES, L. G. F. F. Novela e Sociedade no Brasil. Niterói: EdUFF, 1998.'
   end
+
+  it 'having a subtitle' do
+    ABNTReference.new(
+      author: 'Cláudio Pastro',
+      title: 'Arte Sacra',
+      subtitle: 'Espaço Sagrado Hoje',
+      address: 'São Paulo',
+      publisher: 'Loyola',
+      year: 1993).to_s.should ==
+    'PASTRO, C. Arte Sacra: Espaço sagrado hoje. São Paulo: Loyola, 1993.'
+  end
 end
